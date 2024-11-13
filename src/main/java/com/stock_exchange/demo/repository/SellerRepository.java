b76a4a1ee335eb5b11d8b21f6c55d41bd2ebbdc5
PaulaCiocu <paulafarcas2002@yahoo.com>
2024-11-13 12:41:00 +0200

    Merged branches to reach final code

--- src/main/java/com/stock_exchange/demo/repository/SellerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stock_exchange.demo.repository;
-
-import com.stock_exchange.demo.entity.Seller;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerRepository extends JpaRepository<Seller, Long> {
-}
