f1aeca184bef3b8ce5807b2e0a367ff0e3ee9782
PaulaCiocu <paulafarcas2002@yahoo.com>
2024-11-13 12:32:50 +0200

    Added OpenAPi + Swagger

--- src/main/java/com/stockexchange/demo/StockExchangeApplication.java
+++ src/main/java/com/stockexchange/demo/StockExchangeApplication.java
@@ -3,11 +3,31 @@ package com.stockexchange.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.net.URI;
+
 @SpringBootApplication
 public class StockExchangeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockExchangeApplication.class, args);
+
+		try {
+			String url = "http://localhost:8000/swagger-ui/index.html";
+			String os = System.getProperty("os.name").toLowerCase();
+
+			if (os.contains("win")) {
+				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} else if (os.contains("mac")) {
+				Runtime.getRuntime().exec("open " + url);
+			} else if (os.contains("nix") || os.contains("nux")) {
+				Runtime.getRuntime().exec("xdg-open " + url);
+			} else {
+				System.err.println("Unsupported OS, cannot open browser automatically.");
+			}
+		} catch (Exception e) {
+			System.err.println("Failed to open Swagger UI: " + e.getMessage());
+		}
 	}
 
 }
